37f34fd10d2c868c23cafcaf980a074a36d7178f
NeoSoleil <126864523+NeoSolleil@users.noreply.github.com>
2024-02-26 09:03:47 +0900

    Update kuhaku_3wo1.py

--- ベロティカ/kuhaku_3wo1.py
+++ ベロティカ/kuhaku_3wo1.py
@@ -1,6 +1,6 @@
 
 
-file_path = '/home/neosoleil/遊び/ベロティカ/歌詞分かち書き/覚醒butterfly.txt_tokenized.txt'  # テキストファイルのパスを適切に指定してください
+file_path = ''  # テキストファイルのパスを適切に指定してください
 
 # ファイルを開いて中のデータを読み込む
 with open(file_path, 'r') as file:
